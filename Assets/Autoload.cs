4485f4fa422be4250596bde6549e366c66c71c89
streeeloi <streeeloi@gmail.com>
2019-08-19 22:46:08 +0200

    Add transtion on scenes

---
+++ Assets/Autoload.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Autoload : MonoBehaviour
+{
+    public int levelIndex=1;
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Application.LoadLevel(levelIndex);
+    }
+
+
+    public void LoadNext() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            LoadNext();
+        }
+    }
+}
